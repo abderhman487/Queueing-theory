933b6b6a4927a229874821cf7bbc4aebb0381249
Abderhman Gamal <abderhmangamal246@gmail.com>
2025-12-04 02:55:17 +0200

    adding ti_calculation method

---
+++ helpers_functions/ti_calc.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+'''
+Deteremining the ti (the smallest real number that satisfying the used equation)
+for a queue D/D/1/(K-1)/FIFO
+'''
+def ti_calc(lam:np.double, mu:np.double, M:int, k:int):
+
+    if lam > mu:
+        # ti is when n(t) == k+1 because k is the max value rhe queue can handle
+        k = k+1
+
+        approximate_ti = (k-(mu/lam)) / (lam-mu)
+        t_start = int(max(1, approximate_ti - 10)) #max(1,ti) because ti - 10 could be negative
+        t_end = int(approximate_ti + 20)
+
+        for t in range(t_start,t_end):
+            n_t = np.floor(t*lam) - np.floor((mu*t)-(mu/lam))
+            if n_t == k:
+                return t
+            
+    if mu > lam:
+        # ti is when n(t) == 0
+
+        approximate_ti = M / (mu - lam)
+        t_start = int(max(1, approximate_ti - 10))
+        t_end = int(approximate_ti + 20)
+
+        for t in range(t_start,t_end):
+            n_t = M + np.floor(t*lam) - np.floor(mu*t)
+            if n_t == 0:
+                return t
+            
+    if mu == lam:
+        return 0 
